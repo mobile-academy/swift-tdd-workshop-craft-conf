5189a3601a3eee396d203763d1d36c5b7431ad76
Lukasz Warchol <lwarchol@shutterstock.com>
2015-11-20 00:37:14 +0100

    Agenda view controller implemented.

--- TDDWorkshop/Classes/Agenda/AgendaViewController.swift
+++ TDDWorkshop/Classes/Agenda/AgendaViewController.swift
@@ -9,34 +9,41 @@ class AgendaViewController: UITableViewController {
 
     var agendaProvider: AgendaProviding!
 
-    // MARK: Initializers
-    
+    // MARK: Initialisers
+
     override init(style: UITableViewStyle) {
         super.init(style: style)
         self.agendaProvider = AgendaProvider()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.agendaProvider = AgendaProvider()
     }
-    
+
     // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // TODO: reload agenda
+
+        self.agendaProvider.reloadAgenda { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
 
     // MARK: Table view
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0;
+        return self.agendaProvider.agendaItems.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let agendaItem = self.agendaProvider.agendaItems[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: AgendaItemCellIdentifier, for: indexPath) as! AgendaItemCell
+
+        cell.timeLabel.text = agendaItem.time
+        cell.nameLabel.text = agendaItem.name
+
         return cell;
     }
 }
