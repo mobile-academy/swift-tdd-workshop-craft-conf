b2e08154cb3f546d60b06a4e3e99fa8a33dbba31
Aleksander Zubala <alek@zubala.com>
2017-04-21 19:28:09 +0200

    Added Firebase/Core

--- Pods/Target Support Files/Pods-TDDWorkshop/Pods-TDDWorkshop-frameworks.sh
+++ Pods/Target Support Files/Pods-TDDWorkshop/Pods-TDDWorkshop-frameworks.sh
@@ -90,10 +90,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/UIImage-Resize/UIImage_Resize.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/UIImage-Resize/UIImage_Resize.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- TDDWorkshop/Classes/AppDelegate.swift
+++ TDDWorkshop/Classes/AppDelegate.swift
@@ -3,25 +3,17 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    // MARK: UIApplicationDelegate
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        configureApplication(launchOptions)
+        MobileAcademyStyleSheet.applyStyle()
+        FIRApp.configure()
         return true
-    }
-
-    // MARK: Helpers
-
-    func configureApplication(_ launchOptions: [AnyHashable: Any]?) {
-        let configurator = Configurator()
-        let appConfiguration = ConfigurationFactory().applicationConfiguration()
-        configurator.configureApplication(appConfiguration, launchOptions: launchOptions)
     }
 }
 
